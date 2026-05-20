5843e6bdec40d924979dc6e836e5e94ea4344406
Marcio Momberger <marcio.momberger@liberato.com.br>
2026-05-20 15:12:12 -0300

    aula2005

---
+++ aula15/script.js
@@ -0,0 +1,4 @@
+function alterarCor() {
+    document.body.style.backgroundColor = "black";
+    document.getElementById("titulo").style.color="white";
+}
---
+++ aula16/desafio3/script.js
@@ -0,0 +1,37 @@
+function adicionarProduto() {
+    // pega os valores dos campos
+    let nome = document
+    .getElementById("nome")
+    .value;
+    let preco = document
+    .getElementById("preco")
+    .value;
+    // validação
+    if(nome == "" || preco == "") {
+        alert("Preencha todos os campos");
+        return;
+    }
+    // seleciona tabela
+    let tabela = document
+    .getElementById("tabelaProdutos");
+    // cria linha
+    let linha = document
+    .createElement("tr");
+    // cria coluna nome
+    let colunaNome = document
+    .createElement("td");
+    colunaNome.innerHTML = nome;
+    // cria coluna preço
+    let colunaPreco = document
+    .createElement("td");
+    colunaPreco.innerHTML =
+    "R$ " + parseFloat(preco).toFixed(2);
+    // adiciona colunas na linha
+    linha.appendChild(colunaNome);
+    linha.appendChild(colunaPreco);
+    // adiciona linha na tabela
+    tabela.appendChild(linha);
+    // limpa os campos
+    document.getElementById("nome").value = "";
+    document.getElementById("preco").value = "";
+}
